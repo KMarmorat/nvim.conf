dc9c7357bc13dc7f167d9549bca3b8611fe9f741
Kenzo <kenzo@2a02-8428-1972-2801-731e-6ab7-cce4-acd7.rev.sfr.net>
2023-08-25 17:24:55 +0200

    Added good looking solution

---
+++ lua/kenzo/set.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_set_hl(0,"Comment",{italic=true})
+vim.opt.termguicolors = true
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,20 +1,51 @@
 return {
-	{
+  {
 		"cljoly/bepo.nvim",
 		config = true
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup()
+		end,
 	},
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		keys = {
-			{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("neo-tree").setup()
-		end,
-	}
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function () 
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "julia" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true, disable = { "yaml" } },
+				rainbow = {
+					enable = true,
+					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+					max_file_lines = nil, -- Do not enable for files with more than n lines, int
+					-- colors = {}, -- table of hex strings
+					-- termcolors = {} -- table of colour name strings
+				}
+			})
+		end
+	},
+	{
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function ()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+  },
 }
